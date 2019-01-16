d78f528606b96a1507f53b1b77470b04c2bb48da
David Chesnut <davech@microsoft.com>
2019-01-15 17:13:38 -0800

    Initial commit AsyncStorage sample

---
+++ Excel-custom-functions/AsyncStorage/Home.js
@@ -0,0 +1,12 @@
+(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded.
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            
+           
+        });
+    };
+
+})();
---
+++ Excel-custom-functions/AsyncStorage/src/customfunctions.js
@@ -0,0 +1,21 @@
+﻿
+
+function add(first, second){
+  return first + second;
+}
+
+function StoreValue(key, value) {
+  return OfficeRuntime.AsyncStorage.setItem(key, value).then(function (result) {
+      return "Success: Item with key '" + key + "' saved to AsyncStorage.";
+  }, function (error) {
+      return "Error: Unable to save item with key '" + key + "' to AsyncStorage. " + error;
+  });
+}
+
+function GetValue(key) {
+  return OfficeRuntime.AsyncStorage.getItem(key);
+}
+
+CustomFunctions.associate("ADD", add);
+CustomFunctions.associate("STOREVALUE",StoreValue);
+CustomFunctions.associate("GETVALUE",GetValue);
