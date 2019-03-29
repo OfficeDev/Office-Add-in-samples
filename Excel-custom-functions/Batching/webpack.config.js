a6693ebee0e8458c5078d68dab78424781963049
David Chesnut <davech@microsoft.com>
2019-03-29 14:39:43 -0700

    updated config

--- Excel-custom-functions/Batching/webpack.config.js
+++ Excel-custom-functions/Batching/webpack.config.js
@@ -72,9 +72,6 @@ module.exports = async (env, options) => {
       }),
     ],
     devServer: {
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      },
       https: await devCerts.getHttpsServerOptions(),
       port: 3000
     }
