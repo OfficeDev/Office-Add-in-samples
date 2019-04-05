163a6ab87d68a7cc370c12ac405cae94632fc9a4
Michelle Scharlock <mischarl@microsoft.com>
2019-04-05 16:43:21 -0700

    Custom Functions batching files/folders (#32)
    * adding batching folders/files
    
    * completed file
    
    * Code review from Linda
    
    * updated with running project code
    
    * updating copyrights
    
    * update per Linda's feedback
    
    * update readme and trim to only batch related cf
    
    * method name fix
    
    * added security notes
    
    * minor change
    
    * updated to latest template and typescript
    
    * updated readme security note
    
    * updated config
    
    * Refactor from Michaelz
    
    * adding operation error scenario
    
    * restoring header
    
    * fixed bug causing #VALUE to display
    
    * date fix

---
+++ Excel-custom-functions/Batching/README.md
@@ -0,0 +1,94 @@
+# Custom function batching pattern #
+
+### Summary ###
+If your custom functions call a remote service you may want to use a batching pattern to reduce the number of network calls to the remote service. This is useful when a spreadsheet recalculates and it contains many of your custom functions. Recalculate will result in many calls to your custom functions, but you can batch them into one or a few calls to the remote service. 
+
+### Applies to ###
+-  Custom functions on Excel desktop and online
+
+### Prerequisites ###
+Custom functions are currently in developer preview. To get set up and working with custom functions, see [Custom functions requirements](https://docs.microsoft.com/en-us/office/dev/add-ins/excel/custom-functions-requirements)
+
+### Solution ###
+Solution | Author(s)
+---------|----------
+Custom function batching | Microsoft
+
+### Version history ###
+Version  | Date | Comments
+---------| -----| --------
+1.0  | April 5, 2019 | Initial release
+
+### Disclaimer ###
+**THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.**
+
+
+----------
+
+# Scenario: Custom function batching #
+In this scenario your custom functions call a remote service. To reduce network round trips you will batch all the calls and send them in a single call to the web service. This is ideal when the spreadsheet is recalculated. For example, if someone used your custom function in 100 cells in a spreadsheet, and then recalculates the spreadsheet, your custom function would run 100 times and make 100 network calls. By using this batching pattern, the calls can be combined to make all 100 calculations in a single network call.
+
+## Run the sample
+To run this sample, download the code and go to the **Batching** folder in a command prompt window.
+
+1. Run `npm install`.
+2. Run `npm run build`.
+3. Run `npm run start`. The sample will now sideload into Excel on desktop.
+
+## Key parts of the sample
+The code pattern contains two custom functions named `DIV2` and `MUL2`. Instead of performing the calculation, each of them calls a `_pushOperation` function to push the operation into a batch queue to be passed to a web service.
+
+```typescript
+function mul2(first: number, second: number) {
+  return _pushOperation(
+    "mul2",
+    [first, second]
+  );
+}
+```
+
+### Batching the operation
+The `_pushOperation` function pushes each operation into a _batch variable. It schedules the batch call to be made within 100 milliseconds. You can adjust this when using the code in your own solution.
+
+```typescript
+  // If a remote request hasn't been scheduled yet,
+  // schedule it after a certain timeout, e.g. 100 ms.
+  if (!_isBatchedRequestScheduled) {
+    _isBatchedRequestScheduled = true;
+    setTimeout(_makeRemoteRequest, 100);
+  }
+```
+
+### Making the remote request
+The `_makeRemoteRequest` function prepares the batch request and passes it to the `_fetchFromRemoteService` function. If you are adapting this code to your own solution you need to modify `_makeRemoteRequest` to actually call your remote service.
+
+### The remote service
+The `_fetchFromRemoteService` function processes the batch of operations, performs the operations, and then returns the results. In this sample, `_fetchFromRemoteService` is just another function to demonstrate the pattern. When adapting this code to your solution, use this method on the server-side to respond to the client call over the network.
+
+## How to apply batching in your own solution
+You can copy and paste this code into your own solution. When using this pattern, you'll need to evaluate and update the following areas of code.
+
+### _pushOperation
+Adjust the timeout value as needed. A longer time will be more noticable to the user. A shorter time may result in more calls to the remote service.
+
+### _makeRemoteRequest
+Modify this function to actually make a network call to your remote service and pass the batch operations in a single call. For example, you may want to serialize the batch entries into a JSON body to be passed in the netwwork call to the remote service.
+
+### _fetchFromRemoteService
+Place this function in your remote service to handle the network call from the client. You'll want to modify this to perform the actual operations of your custom functions (or call the correct methods to do so.)
+
+**Note**: You should remove the call to `pause(1000)` which simulates network latency in the sample.
+
+## Security notes
+When implementing the **_fetchFromRemoteService** function on a server, apply an appropriate authentication mechanism. Ensure that only the correct callers can access the function.
+
+In the webpack.config.js file, a header is set to  `"Access-Control-Allow-Origin": "*"`. This is only for development purposes. You should lock this header down to only allowed domains in production code.
+
+You will be prompted to install self-signed certificates when you run this sample on your development computer. The certificates are intended only for running and studying this code sample. Do not reuse them in your own code solutions or in production environments.
+
+You can install or uninstall the self-signed certificates by running the following commands in the project folder.
+
+```cli
+npx office-addin-dev-certs install
+npx office-addin-dev-certs uninstall
+```
---
+++ Excel-custom-functions/Batching/src/functions/functions.ts
@@ -0,0 +1,175 @@
+﻿/**
+ * @CustomFunction
+ * Adds two numbers without using batching
+ * @param first First number
+ * @param second Second number
+ * @returns The sum of the two numbers.
+ */
+function addNoBatch(first: number, second: number): number {
+  return first + second;
+}
+
+/**
+ * @CustomFunction
+ * Divides two numbers using batching
+ * @param dividend The number being divided
+ * @param divisor The number to divide the dividend with
+ * @returns The sum of the two numbers
+ */
+function div2(dividend: number, divisor: number) {
+  return _pushOperation(
+    "div2",
+    [dividend, divisor]
+  );
+}
+
+/**
+ * @CustomFunction
+ * Multiplies two numbers together using batching
+ * @param first First number to multiply
+ * @param second Second number to multiply
+ * @returns The product of the two numbers
+ */
+function mul2(first: number, second: number) {
+  return _pushOperation(
+    "mul2",
+    [first, second]
+  );
+}
+
+/**
+ * Defines the implementation of the custom functions
+ * for the function id defined in the metadata file (functions.json).
+ */
+CustomFunctions.associate("ADDNOBATCH", addNoBatch);
+CustomFunctions.associate("DIV2", div2);
+CustomFunctions.associate("MUL2", mul2);
+
+///////////////////////////////////////
+
+// Next batch
+interface IBatchEntry {
+  operation: string;
+  args: any[];
+  resolve: (data: any) => void;
+  reject: (error: Error) => void;
+}
+
+interface IServerResponse {
+  result?: any;
+  error?: string;
+}
+
+const _batch: IBatchEntry[] = [];
+let _isBatchedRequestScheduled = false;
+
+// This function encloses your custom functions as individual entries,
+// which have some additional properties so you can keep track of whether or not
+// a request has been resolved or rejected.
+function _pushOperation(op: string, args: any[]) {
+  // Create an entry for your custom function.
+  const invocationEntry: IBatchEntry = {
+    operation: op, // e.g. sum
+    args: args,
+    resolve: undefined,
+    reject: undefined,
+  };
+
+  // Create a unique promise for this invocation,
+  // and save its resolve and reject functions into the invocation entry.
+  const promise = new Promise((resolve, reject) => {
+    invocationEntry.resolve = resolve;
+    invocationEntry.reject = reject;
+  });
+
+  // Push the invocation entry into the next batch.
+  _batch.push(invocationEntry);
+
+  // If a remote request hasn't been scheduled yet,
+  // schedule it after a certain timeout, e.g. 100 ms.
+  if (!_isBatchedRequestScheduled) {
+    _isBatchedRequestScheduled = true;
+    setTimeout(_makeRemoteRequest, 100);
+  }
+
+  // Return the promise for this invocation.
+  return promise;
+}
+
+// This is a private helper function, used only within your custom function add-in.
+// You wouldn't call _makeRemoteRequest in Excel, for example.
+// This function makes a request for remote processing of the whole batch,
+// and matches the response batch to the request batch.
+function _makeRemoteRequest() {
+  // Copy the shared batch and allow the building of a new batch while you are waiting for a response.
+  // Note the use of "splice" rather than "slice", which will modify the original _batch array
+  // to empty it out.
+  const batchCopy = _batch.splice(0, _batch.length);
+  _isBatchedRequestScheduled = false;
+
+  // Build a simpler request batch that only contains the arguments for each invocation.
+  const requestBatch = batchCopy.map((item) => {
+    return { operation: item.operation, args: item.args };
+  });
+
+  // Make the remote request.
+  _fetchFromRemoteService(requestBatch)
+    .then((responseBatch) => {
+      // Match each value from the response batch to its corresponding invocation entry from the request batch,
+      // and resolve the invocation promise with its corresponding response value.
+      responseBatch.forEach((response, index) => {
+        if (response.error) {
+          batchCopy[index].reject(new Error(response.error));
+        } else {
+          console.log(response);
+          batchCopy[index].resolve(response.result);
+        }
+      });
+    });
+}
+
+// --------------------- A public API ------------------------------
+
+// This function simulates the work of a remote service. Because each service
+// differs, you will need to modify this function appropriately to work with the service you are using. 
+// This function takes a batch of argument sets and returns a [promise of] batch of values.
+// NOTE: When implementing this function on a server, also apply an appropriate authentication mechanism
+//       to ensure only the correct callers can access it.
+async function _fetchFromRemoteService(
+  requestBatch: Array<{ operation: string, args: any[] }>
+): Promise<IServerResponse[]> {
+  // Simulate a slow network request to the server;
+  await pause(1000);
+
+  return requestBatch.map((request): IServerResponse => {
+    const { operation, args } = request;
+
+    try {
+      if (operation === "div2") {
+        // Divide the first argument by the second argument.
+        return {
+          result: args[0] / args[1]
+        };
+      } else if (operation === "mul2") {
+        // Multiply the arguments for the given entry.
+        const myresult = args[0] * args[1];
+        console.log(myresult);
+        return {
+          result: myresult
+        };
+      } else {
+        return {
+          error: `Operation not supported: ${operation}`
+        };
+      }
+    } catch (error) {
+      return {
+        error: `Operation failed: ${operation}`
+      };
+    }
+  });
+}
+
+function pause(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
