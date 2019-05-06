85258639e5fcc4952a631c5cb455f01d32719e0a
David Chesnut <davech@microsoft.com>
2019-05-06 16:08:24 -0700

    rebuilt template

--- Excel-custom-functions/Storage/src/functions/functions.js
+++ Excel-custom-functions/Storage/src/functions/functions.js
@@ -2,7 +2,8 @@
 // Licensed under the MIT License.
 
 /**
- * Adds two numbers together. 
+ * @customfunction
+ * @description Adds two numbers together. 
  * @param {number} first First number to be added.
  * @param {number} second Second number to be added.
  */
@@ -11,12 +12,13 @@ function add(first, second){
 }
 
 /**
- * Stores a value in Office.storage.
+ * @customfunction
+ * @description Stores a value in Office.storage.
  * @param {any} key Key in the key-value pair you will store. 
  * @param {any} value Value in the key-value pair you will store. 
  */
 function StoreValue(key, value) {
-  return Office.storage.setItem(key, value).then(function (result) {
+  return OfficeRuntime.storage.setItem(key, value).then(function (result) {
       return "Success: Item with key '" + key + "' saved to storage.";
   }, function (error) {
       return "Error: Unable to save item with key '" + key + "' to storage. " + error;
@@ -24,11 +26,12 @@ function StoreValue(key, value) {
 }
 
 /**
- * Gets value from Office.storage. 
+ * @customfunction
+ * @description Gets value from Office.storage. 
  * @param {any} key Key of item you intend to get.
  */
 function GetValue(key) {
-  return Office.storage.getItem(key);
+  return OfficeRuntime.storage.getItem(key);
 }
 
 CustomFunctions.associate("ADD", add);
