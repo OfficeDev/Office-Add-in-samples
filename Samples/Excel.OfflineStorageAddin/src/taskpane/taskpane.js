007bb3ec977499f68bdbe09ef29024632def30c5
Nancy Wang <nwang22@jhu.edu>
2019-07-19 16:35:41 -0700

    Offline storage (#51)
    *  create folder for offline storage docs
    
    *  create folder for offline storage docs
    
    * Initial upload of sample code
    
    * copy template PNP markdown to README
    
    * update to add-in to run in local server
    
    * describe usage and use case of code sample
    
    * add gitignore file
    
    * update scenario description
    
    * anonymized provider name in manifest
    
    * add code samples and explanations
    
    * fix hyperlink issue
    
    * Apply suggestions from code review
    
    Co-Authored-By: Michelle Scharlock <mischarl@microsoft.com>
    
    * Apply suggestions from code review
    
    Co-Authored-By: Michelle Scharlock <mischarl@microsoft.com>
    
    * reformat code snippets
    
    * remove references to caching, add security details
    
    * add security detail to code comments
    
    * remove ref to caching
    
    * instructions for simulating offline, remove jquery
    
    * Changed the ajax call and aded comments
    
    * Delete taskpane.js
    
    * Update and rename Home.js to taskpane.js
    
    * Delete sampleData.js
    
    * update json with fictitious names
    
    * anonymize manifest provider name
    
    * update README to reflect code changes
    
    * update telemetry image
    
    * update jquery pkg, remove NBA ref
    
    * add copyright comment
    
    * add copyright comment
    
    * add additional npm command
    
    * update build comments to reflect PR feedback
    
    * Update README.md
    
    * add copyright info
    
    * fixing some typos
    
    * shorten up applies to
    
    * provide resources to check IndexedDB compat
    
    * Add metadata and a few other adjustments

---
+++ Samples/Excel.OfflineStorageAddin/src/taskpane/taskpane.js
@@ -0,0 +1,78 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+'use strict';
+
+(function () {
+
+    // Office is ready
+    Office.onReady(function () {
+        // The document is ready
+        $(document).ready(function () {
+
+            // Assign HTML buttons to functions
+            $('#create-table').click(loadTable);
+        });
+    });
+    
+    // Loads data for a table from the server if connected
+    // or local storage if disconnected and had been loaded before
+    function loadTable() {
+        $.ajax({
+            dataType: "json",
+            url: "sampleData.json",
+            success: function (result, status, xhr) {
+                // Stores the JSON retrieved from the AJAX call as a string in
+                // local storage under the key "PlayerData"
+                localStorage.PlayerData = JSON.stringify(result);
+
+                // Sends the new data to the table
+                createTable(result);
+            },
+            error: function (xhr, status, error) {
+                // If the connections fails, checks if "PlayerData" was previously stored in local storage
+                if (localStorage.PlayerData) {
+                    // Retrieves the string saved earlier under the key "PlayerData"
+                    // and parses it into an object
+                    let dataObject = JSON.parse(localStorage.PlayerData);
+
+                    // Sends the saved data to the table
+                    createTable(dataObject);
+                }
+                else {
+                    console.log("Player data failed to load with error: " + error);
+                }
+            }
+        });
+    }
+    
+    // Creates and populates table of stats from sampleData.json
+    function createTable(playerData) {
+        Excel.run(function (context) {
+            let currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+            let playerTable = currentWorksheet.tables.getItemOrNullObject('PlayerTable');
+
+            return context.sync().then(function () {
+                playerTable.delete();
+
+                playerTable = currentWorksheet.tables.add("A1:D1", true);
+                playerTable.name = "PlayerTable";
+
+                playerTable.getHeaderRowRange().values =
+                    [["Name", "PPG", "Rebounds", "APG"]];
+
+                playerTable.rows.add(null, playerData);
+
+                playerTable.getRange().format.autofitColumns();
+                playerTable.getRange().format.autofitRows();
+
+                return context.sync();
+            });
+        }).catch(function (error) {
+            console.log("Error: " + error);
+            if (error instanceof OfficeExtension.Error) {
+                console.log("Debug info: " + JSON.stringify(error.debugInfo));
+            }
+        });
+    }
+                    
+})();
