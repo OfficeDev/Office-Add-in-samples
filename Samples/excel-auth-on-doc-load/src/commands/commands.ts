0322eb862c740925d31b0ff660457ecaef9d9297
David Chesnut <davech@microsoft.com>
2020-02-09 16:34:59 -0800

    added additional buttons

--- Samples/excel-auth-on-doc-load/src/commands/commands.ts
+++ Samples/excel-auth-on-doc-load/src/commands/commands.ts
@@ -17,6 +17,9 @@ const g = getGlobal() as any;
 
 // the add-in command functions need to be available in global scope
 g.btnsignin = btnSignIn;
+g.btnsignout = btnSignOut;
+g.btnenableaddinstart = btnEnableAddinStart;
+g.btndisableaddinstart = btnDisableAddinStart;
 
 export function btnSignIn(event: Office.AddinCommands.Event) {
     console.log('sign in button pressed');
@@ -26,5 +29,26 @@ export function btnSignIn(event: Office.AddinCommands.Event) {
     signInO365(signapp.setState, signapp.setToken, signapp.displayError);
     //SetRuntimeVisibleHelper(true);
     // Be sure to indicate when the add-in command function is complete
+    event.completed();
+}
+
+export function btnSignOut(event: Office.AddinCommands.Event) {
+    console.log('sign out button pressed');
+    // Your code goes here
+
+    event.completed();
+}
+
+export function btnEnableAddinStart(event: Office.AddinCommands.Event) {
+    console.log('Enable add-in start button pressed');
+    // Your code goes here
+
+    event.completed();
+}
+
+export function btnDisableAddinStart(event: Office.AddinCommands.Event) {
+    console.log('Disable add-in start button pressed');
+    // Your code goes here
+
     event.completed();
 }
