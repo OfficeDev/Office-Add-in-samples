5a0d976cb61eac62ee66c91c22e86c9fb730e833
David Chesnut <davech@microsoft.com>
2021-02-02 14:17:48 -0800

    initial commit for contextual tabs sample

---
+++ Samples/office-contextual-tabs/src/commands/commands.js
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+import { createSampleWorkSheet, getTableData, setSyncButtonEnabled } from '../utilities/utilities.js';
+import { getContextualRibbonJSON } from '../commands/ribbonJSON.js';
+
+/* global global, self, window, console */
+
+export function getGlobal() {
+  return typeof self !== "undefined"
+    ? self
+    : typeof window !== "undefined"
+    ? window
+    : typeof global !== "undefined"
+    ? global
+    : undefined;
+}
+
+/**
+ * Handles all ribbon events from the Contoso contextual tab
+ * @param  {} event: The event that was raised.
+ */
+function runRibbonAction(event){
+  switch(event.source.id){
+    case "btnSubmit": runSubmitAction();
+        break;
+    case "btnRefresh": runRefreshAction();
+        break;
+    case "itmExternalExcel": runImportExternalExcelFile();
+        break;
+    case "itmSQLSource": runImportSQLDatabase();
+        break;
+    default: console.log('Event ID: ' + event.source.id + ' was sent, but there is no function handler.');
+    }
+  event.completed();
+}
+
+/**
+ * Submit data changes in table to data source
+ */
+function runSubmitAction(){
+  //Depending on which data source is in use, get data from the table, then update the mock data source.
+  if (g.mockDataSource==='sqlMockData'){
+    getTableData().then ((response) => {g.sqlMockData.data = response});
+  } else if (g.mockDataSource==='excelFileMockData'){
+    getTableData().then ((response) => {g.excelFileMockData.data = response}); 
+  }
+  //Turn off the Refresh and Submit buttons now that the table is in sync with data source.
+  setSyncButtonEnabled(false);
+  g.isTableDirty = false;
+}
+
+/**
+ * Refresh the data in the table from the data source.
+ */
+function runRefreshAction(){
+  //Easy way is to just recreate the worksheet and sales data from source
+  createSampleWorkSheet(g.mockDataSource);
+  g.isTableDirty = false;
+}
+
+/**
+ * Import data from mock External Excel file data source
+ */
+function runImportExternalExcelFile(){
+  g.mockDataSource = 'excelFileMockData';
+  //Just recreate the worksheet using the Excel file mock data source
+  createSampleWorkSheet('excelFileMockData');
+}
+
+/**
+ * Import data from mock SQL database source
+ */
+function runImportSQLDatabase(){
+  g.mockDataSource = 'sqlMockData';
+  //Just recreate the worksheet using the SQL database source
+  createSampleWorkSheet(g.mockDataSource); 
+}
+
+// the add-in command functions need to be available in global scope
+// Globals
+const g = getGlobal();
+
+let excelFileMockData = {data: [
+  ["Frames", 5000, 7000, 6544, 4377],
+  ["Saddles", 400, 323, 276, 651],
+  ["Brake levers", 12000, 8766, 8456, 9812],
+  ["Chains", 1550, 1088, 692, 853],
+  ["Mirrors", 225, 600, 923, 544],
+  ["Spokes", 6005, 7634, 4589, 8765]
+]};
+
+let sqlMockData = {data: [
+  ["Frames", 10, 70, 654, 437],
+  ["Saddles", 4000, 3230, 2760, 6510],
+  ["Brake levers", 2000, 766, 456, 812],
+  ["Chains", 150, 188, 62, 83],
+  ["Mirrors", 25, 60, 93, 54],
+  ["Spokes", 605, 734, 489, 875]
+]};
+
+g.contextualTab = getContextualRibbonJSON();
+g.excelFileMockData = excelFileMockData;
+g.sqlMockData = sqlMockData;
+g.runRibbonAction = runRibbonAction;
+g.mockDataSource = 'sqlMockData';
+g.isTableSelected = false;
+g.isTableDirty = false;
---
+++ Samples/office-contextual-tabs/src/utilities/utilities.js
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+import { getGlobal } from "../commands/commands.js";
+
+/* global Excel, Office, console */
+
+/**
+ * Delete the sample worksheet with sales table
+ */
+async function deleteSampleWorkSheet() {
+  return Excel.run(async context => {
+    context.workbook.worksheets.getItemOrNullObject("Sample").delete();
+  });
+}
+
+/**
+ * Create a sample worksheet with sales data table. If the worksheet already exists,
+ * replace it.
+ * @param  {string} mockDataSource Identifies which mock data source to use to create the table.
+ */
+export async function createSampleWorkSheet(mockDataSource) {
+  //Turn off the Refresh and Submit buttons.
+  setSyncButtonEnabled(false);
+
+  let g = getGlobal();
+  g.isTableDirty = false; //reset dirty flag for new table
+
+  //Ensure that the sample worksheet is deleted.
+  await deleteSampleWorkSheet();
+  Excel.run(async context => {
+    //Create sample worksheet
+    const sheet = context.workbook.worksheets.add("Sample");
+
+    //Insert title row above table
+    let range = sheet.getRange("A1");
+    if (mockDataSource === "sqlMockData") {
+      range.values = [["Data source: SQL Database"]];
+    } else {
+      range.values = [["Data source: External Excel File"]];
+    }
+    range.format.autofitColumns();
+
+    //Add table with sales data
+    let salesTable = sheet.tables.add("A2:E2", true);
+    salesTable.name = "SalesTable";
+    salesTable.onSelectionChanged.add(onSelectionChange);
+
+    g.tableEventCount = 4; //This is to track and ignore the 4 events that will be generated from the next few lines of code.
+
+    //add an onChanged event handler
+    salesTable.onChanged.add(onChanged);
+
+    //Add table header
+    salesTable.getHeaderRowRange().values = [["Product", "Qtr1", "Qtr2", "Qtr3", "Qtr4"]];
+
+    //Add data rows depending on which data source is in use.
+    if (mockDataSource === "sqlMockData") {
+      salesTable.rows.add(null, g.sqlMockData.data);
+    } else if (mockDataSource === "excelFileMockData") {
+      salesTable.rows.add(null, g.excelFileMockData.data);
+    }
+
+    sheet.getUsedRange().format.autofitColumns();
+    sheet.getUsedRange().format.autofitRows();
+
+    sheet.activate();
+    sheet.getRange("A2").select();
+    await context.sync();
+
+    //creating is done
+    g.isTableCreating = false;
+    return context.sync();
+  });
+}
+
+/**
+ * Get the Sales table data and return as Promise in an array.
+ */
+export async function getTableData() {
+  let response = null;
+
+  return Excel.run(async context => {
+    const sheet = context.workbook.worksheets.getItem("Sample");
+    const expensesTable = sheet.tables.getItem("SalesTable");
+    const bodyRange = expensesTable.getDataBodyRange().load("values");
+    await context.sync();
+
+    response = bodyRange.values;
+    return response;
+  });
+}
+
+/**
+ * Handles the onSelectionChange event. If selection is inside the table, the Contoso custom tab is shown.
+ * Otherwise the Contoso custom tab is hidden.
+ * @param  {} args The arguments for the selection changed event.
+ */
+function onSelectionChange(args) {
+  let g = getGlobal();
+  setContextualTabVisibility(args.isInsideTable);
+  g.isTableSelected = args.isInsideTable;
+}
+
+/**
+ * Handles the onChanged event. When data in the sales table is changed,
+ * enable the refresh and submit buttons.
+ */
+function onChanged() {
+  let g = getGlobal();
+
+  //When the add-in creates the table, it will generate 4 events that we must ignore.
+  //We only want to respond to the change events from the user.
+  if (g.tableEventCount > 0) {
+    g.tableEventCount--;
+    return; //count down to throw away events caused by the table creation code
+  }
+
+  //check if dirty flag was set (flag avoids extra unnecessary ribbon operations)
+  if (!g.isTableDirty) {
+    g.isTableDirty = true;
+
+    //Enable the Refresh and Submit buttons
+    setSyncButtonEnabled(true);
+  }
+}
+
+/**
+ * Shows or hides the contextual tab for Contoso depending on the visible parameter.
+ * @param  {} visible true if the contextual tab is to be shown; otherwise, false.
+ */
+function setContextualTabVisibility(visible) {
+  let g = getGlobal();
+  g.contextualTab.tabs[0].visible = visible;
+  try {
+    Office.ribbon.requestUpdate(g.contextualTab);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+/**
+ * Enables or disables the Refresh and Submit buttons for table data.
+ *
+ * @param  {boolean} visible true if the buttons should be enabled; otherwise, false.
+ */
+export function setSyncButtonEnabled(visible) {
+  let g = getGlobal();
+  g.contextualTab.tabs[0].groups[1].controls[0].enabled = visible;
+  g.contextualTab.tabs[0].groups[1].controls[1].enabled = visible;
+  Office.ribbon.requestUpdate(g.contextualTab);
+}
