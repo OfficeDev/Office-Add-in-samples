318684afc653c41c10d46779c67f576ad2584d3b
David Chesnut <davech@microsoft.com>
2022-09-01 11:16:48 -0700

    fix todo number

--- Samples/auth/Office-Add-in-NodeJS-SSO/Begin/routes/getFilesRoute.js
+++ Samples/auth/Office-Add-in-NodeJS-SSO/Begin/routes/getFilesRoute.js
@@ -7,7 +7,7 @@ const authHelper = require("../server-helpers/obo-auth-helper");
 const getGraphData = require("../server-helpers/msgraph-helper");
 const jwt = require("jsonwebtoken");
 
-// TODO 12: Add route for /getuserfilenames REST API
+// TODO 11: Add route for /getuserfilenames REST API
 
 
 module.exports = router;
