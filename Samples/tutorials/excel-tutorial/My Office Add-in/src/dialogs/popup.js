31b5d619ba46f8224094f072d235a0d52666a114
Alex Jerabek <aljerabe@microsoft.com>
2023-01-13 15:40:01 -0800

    Address dialog onReady suggestion

--- Samples/tutorials/excel-tutorial/My Office Add-in/src/dialogs/popup.js
+++ Samples/tutorials/excel-tutorial/My Office Add-in/src/dialogs/popup.js
@@ -1,16 +1,19 @@
-(function () {
-    "use strict";
 
-    Office.onReady()
-        .then(function () {
-            document.getElementById("ok-button").onclick = sendStringToParentPage;
-        });
+Office.onReady((info) => {
+    document.getElementById("ok-button").onclick = (() => tryCatch(sendStringToParentPage));
+});
 
-    /**
-     * This sends the text input from the dialog back to the parent.
-     */
-    function sendStringToParentPage() {
-        const userName = document.getElementById("name-box").value;
-        Office.context.ui.messageParent(userName);
+function sendStringToParentPage() {
+    const userName = document.getElementById("name-box").value;
+    Office.context.ui.messageParent(userName);
+}
+
+/** Default helper for invoking an action and handling errors. */
+async function tryCatch(callback) {
+    try {
+        await callback();
+    } catch (error) {
+        // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
+        console.error(error);
     }
-}());
+}
--- Samples/tutorials/excel-tutorial/My Office Add-in/src/taskpane/taskpane.js
+++ Samples/tutorials/excel-tutorial/My Office Add-in/src/taskpane/taskpane.js
@@ -7,11 +7,6 @@
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
-    // Determine if the user's version of Office supports all the Office.js APIs that are used in the tutorial.
-    if (!Office.context.requirements.isSetSupported('ExcelApi', '1.7')) {
-      console.log('Sorry. The tutorial add-in uses Excel.js APIs that are not available in your version of Office.');
-    }
-
     // Assign event handlers and other initialization logic.
     document.getElementById("create-table").onclick = (() => tryCatch(createTable));
     document.getElementById("filter-table").onclick = (() => tryCatch(filterTable));
