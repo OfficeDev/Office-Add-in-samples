0f70bc6231ab2224c513cc44f7a4ee015529584e
David Chesnut <davech@microsoft.com>
2023-04-27 15:56:50 -0700

    Outlook SSO event-based activation sample (#487)
    * Initial commit
    
    * Refactoring work
    
    * Add readme
    
    * fixes from testing
    
    * add .env file
    
    * add placeholders
    
    * Apply suggestions from code review
    
    Co-authored-by: Linda Lu Cannon <lindalu@microsoft.com>
    Co-authored-by: Sam Ramon <15154970+samantharamon@users.noreply.github.com>
    
    * Changes based on review feedback
    
    * minor fixes
    
    * Fixes to work on Outlook on Windows
    
    * Refactor promise chains
    
    * Refactor for promise chains and update readme
    
    * Minor fixes
    
    * Update based on feedback.
    
    * fix event completion handling
    
    ---------
    
    Co-authored-by: Linda Lu Cannon <lindalu@microsoft.com>
    Co-authored-by: Sam Ramon <15154970+samantharamon@users.noreply.github.com>

---
+++ Samples/auth/Outlook-Add-in-SSO-events/app.js
@@ -0,0 +1,67 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// This file is the main Node.js server file that defines the express middleware.
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var getUserProfileRoute = require('./routes/getUserProfile');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+/* Turn off caching when developing. */
+if (process.env.NODE_ENV !== 'production') {
+  app.use(express.static(path.join(__dirname, 'public'),
+                        { etag: false }));
+
+  app.use(function (req, res, next) {
+    res.set({
+      "Content-Security-Policy": "script-src https://appsforoffice.microsoft.com https://ajax.aspnetcdn.com https://alcdn.msauth.net " +  process.env.SERVER_SOURCE,
+      "Cache-Control": "private, no-cache, no-store, must-revalidate",
+      "Expires": "-1",
+      "Pragma": "no-cache"
+    });
+    next()
+  });
+} else {
+  // In production mode, let static files be cached.
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(function (req, res, next) {
+    res.set({
+      "Content-Security-Policy": "script-src https://appsforoffice.microsoft.com https://ajax.aspnetcdn.com https://alcdn.msauth.net " +  process.env.SERVER_SOURCE,
+    });
+    next()
+  });;
+}
+
+app.get('/getUserProfile', getUserProfileRoute);
+
+// Catch 404 error and forward to error handler.
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Error handler.
+app.use(function(err, req, res, next) {
+  // Set locals, only providing error in development.
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Render the error page.
+  res.status(err.status || 500).json({ error: err.message });
+//  res.render('error');
+});
+
+module.exports = app;
