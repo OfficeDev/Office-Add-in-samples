8756d0c58dd583636b8fc899d41b1bc3f003f5d6
Elizabeth Samuel <elizs@microsoft.com>
2023-09-12 12:11:49 -0700

    [Outlook] (tutorial) Add completed version (#594)
    * [Outlook] (tutorial) Add completed version
    
    * Apply suggestions from code review
    
    Co-authored-by: Alex Jerabek <38896772+AlexJerabek@users.noreply.github.com>
    
    * Update excel-tutorial based on outlook-tutorial feedback
    
    ---------
    
    Co-authored-by: Alex Jerabek <38896772+AlexJerabek@users.noreply.github.com>

---
+++ Samples/tutorials/outlook-tutorial/Git the gist/src/commands/commands.js
@@ -0,0 +1,82 @@
+let config;
+let btnEvent;
+
+// The initialize function must be run each time a new page is loaded.
+Office.initialize = function () {
+};
+
+function showError(error) {
+  Office.context.mailbox.item.notificationMessages.replaceAsync('github-error', {
+    type: 'errorMessage',
+    message: error
+  }, function(result){
+  });
+}
+
+let settingsDialog;
+
+function insertDefaultGist(event) {
+
+  config = getConfig();
+
+  // Check if the add-in has been configured.
+  if (config && config.defaultGistId) {
+    // Get the default gist content and insert.
+    try {
+      getGist(config.defaultGistId, function(gist, error) {
+        if (gist) {
+          buildBodyContent(gist, function (content, error) {
+            if (content) {
+              Office.context.mailbox.item.body.setSelectedDataAsync(content,
+                {coercionType: Office.CoercionType.Html}, function(result) {
+                  event.completed();
+              });
+            } else {
+              showError(error);
+              event.completed();
+            }
+          });
+        } else {
+          showError(error);
+          event.completed();
+        }
+      });
+    } catch (err) {
+      showError(err);
+      event.completed();
+    }
+
+  } else {
+    // Save the event object so we can finish up later.
+    btnEvent = event;
+    // Not configured yet, display settings dialog with
+    // warn=1 to display warning.
+    const url = new URI('dialog.html?warn=1').absoluteTo(window.location).toString();
+    const dialogOptions = { width: 20, height: 40, displayInIframe: true };
+
+    Office.context.ui.displayDialogAsync(url, dialogOptions, function(result) {
+      settingsDialog = result.value;
+      settingsDialog.addEventHandler(Office.EventType.DialogMessageReceived, receiveMessage);
+      settingsDialog.addEventHandler(Office.EventType.DialogEventReceived, dialogClosed);
+    });
+  }
+}
+
+// Register the function.
+Office.actions.associate("insertDefaultGist", insertDefaultGist);
+
+function receiveMessage(message) {
+  config = JSON.parse(message.message);
+  setConfig(config, function(result) {
+    settingsDialog.close();
+    settingsDialog = null;
+    btnEvent.completed();
+    btnEvent = null;
+  });
+}
+
+function dialogClosed(message) {
+  settingsDialog = null;
+  btnEvent.completed();
+  btnEvent = null;
+}
