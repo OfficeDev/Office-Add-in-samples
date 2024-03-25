0ee7d5d90cd00ddd625cd016bc7079fb6c21a85a
David Chesnut <davech@microsoft.com>
2024-03-25 10:08:37 -0700

    initial commit

---
+++ Samples/auth/outlook-naa/src/taskpane/taskpane.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global document, Office */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Outlook) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+  }
+});
+
+export async function run() {
+  /**
+   * Insert your Outlook code here
+   */
+
+  const item = Office.context.mailbox.item;
+  document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.subject;
+}
