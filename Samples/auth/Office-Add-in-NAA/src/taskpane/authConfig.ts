0f25d86374c319e77c8f268951f42083a7f731eb
Adam Krantz <adamk@microsoft.com>
2024-03-26 16:57:37 -0700

    more improvements

--- Samples/auth/Office-Add-in-NAA/src/taskpane/authConfig.ts
+++ Samples/auth/Office-Add-in-NAA/src/taskpane/authConfig.ts
@@ -29,33 +29,9 @@ class AccountManager {
     this.pca = await PublicClientNext.createPublicClientApplication(msalConfig);
   }
 
-  /**
-   * Uses MSAL and nested app authentication to get an access token through Office SSO.
-   * Call this function any time you need an access token for Microsoft Graph.
-   *
-   * @returns An access token for calling Microsoft Graph APIs.
-   */
   async ssoGetToken() {
-    if (this.pca === undefined) throw new Error("AccountManager is not initialized!");
-    // Specify minimum scopes needed for the access token.
-    const tokenRequest = {
-      scopes: ["Files.Read"],
-      loginHint: myloginHint,
-    };
-    try {
-      const userAccount = await this.pca.acquireTokenSilent(tokenRequest);
-      return userAccount.accessToken;
-    } catch (error) {
-      // Acquire token silent failure. Send an interactive request via popup.
-      try {
-        const userAccount = await this.pca.acquireTokenPopup(tokenRequest);
-        return userAccount.accessToken;
-      } catch (popupError) {
-        // Acquire token interactive failure.
-        console.log(popupError);
-        throw new Error("Unable to acquire access token: " + popupError);
-      }
-    }
+    const userAccount = this.ssoGetUserIdentity();
+    return (await userAccount).accessToken;
   }
 
   /**
