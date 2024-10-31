f1b2f4ac3bb98fd3cf0285650031ae79664420a3
Herman Wen <105693302+hermanwenhe@users.noreply.github.com>
2024-10-31 17:13:29 +0800

    Update Samples/Word-HelloWorld-TaskPane-JS/src/taskpane/taskpane.js
    Co-authored-by: Alex Jerabek <38896772+AlexJerabek@users.noreply.github.com>

--- Samples/Word-HelloWorld-TaskPane-JS/src/taskpane/taskpane.js
+++ Samples/Word-HelloWorld-TaskPane-JS/src/taskpane/taskpane.js
@@ -22,7 +22,7 @@ export async function run() {
     // Insert a paragraph at the end of the document.
     const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
 
-    // change the paragraph color to blue.
+    // Change the paragraph color to blue.
     paragraph.font.color = "blue";
 
     await context.sync();
