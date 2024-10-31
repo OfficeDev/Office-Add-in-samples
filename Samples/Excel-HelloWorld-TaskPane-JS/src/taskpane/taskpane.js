b082d9c62d093b191bf3192e9cc8886008e45c8d
Herman Wen <105693302+hermanwenhe@users.noreply.github.com>
2024-10-31 17:13:12 +0800

    Update Samples/Excel-HelloWorld-TaskPane-JS/src/taskpane/taskpane.js
    Co-authored-by: Alex Jerabek <38896772+AlexJerabek@users.noreply.github.com>

--- Samples/Excel-HelloWorld-TaskPane-JS/src/taskpane/taskpane.js
+++ Samples/Excel-HelloWorld-TaskPane-JS/src/taskpane/taskpane.js
@@ -21,7 +21,7 @@ export async function run() {
        */
       const range = context.workbook.getSelectedRange();
 
-      // Read the range address
+      // Read the range address.
       range.load("address");
 
       // Update the fill color
