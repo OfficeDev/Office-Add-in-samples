6052a377b89d1fed49f06daf9bbf06acacc41636
David Chesnut <davech@microsoft.com>
2025-03-14 15:38:36 -0700

    remove comment

--- Samples/auth/Outlook-Add-in-SSO-NAA-Identity/SPA/webpack.config.js
+++ Samples/auth/Outlook-Add-in-SSO-NAA-Identity/SPA/webpack.config.js
@@ -3,7 +3,6 @@
 const devCerts = require("office-addin-dev-certs");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-//const nodeExternals = require("webpack-node-externals");
 
 const urlDev = "https://localhost:3000/";
 const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
