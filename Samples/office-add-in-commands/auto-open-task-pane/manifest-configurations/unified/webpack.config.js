0c84de47779a31222559ac1bc3b7ed892104b714
Rick Kirkham <rickeeee@msn.com>
2025-07-29 12:53:12 -0700

    review change

--- Samples/office-add-in-commands/auto-open-task-pane/manifest-configurations/unified/webpack.config.js
+++ Samples/office-add-in-commands/auto-open-task-pane/manifest-configurations/unified/webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://officedev.github.io/Office-Add-in-samples/Samples/hello-world/powerpoint-hello-world/"; 
+const urlProd = "https://officedev.github.io/Office-Add-in-samples/Samples/office-add-in-commands/auto-open-task-pane/"; 
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
