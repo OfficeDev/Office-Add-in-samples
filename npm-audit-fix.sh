65053b93ca60d3f964e0d5e56c877cb5c048cff6
Elizabeth Samuel <elizs@microsoft.com>
2025-08-28 07:49:29 -0700

    Update based on feedback

--- npm-audit-fix.sh
+++ npm-audit-fix.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Define directory name to exclude
+EXCLUDE_DIR="node_modules"
+
 # Define the base directory to search
 BASE_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 echo "Base directory: $BASE_DIR"
@@ -10,7 +13,11 @@ FILE_PATTERN="package-lock.json"
 # Find files and switch to their directories
 find "$BASE_DIR" -type f -name "$FILE_PATTERN" | while read -r FILE; do
   DIR=$(dirname "$FILE")  # Extract the directory path
-  echo "Switching to directory: $DIR"
+  if [[ "$DIR" == *"$EXCLUDE_DIR"* ]]; then
+    echo -e "\e[33mSkipping excluded directory: $DIR\e[0m"
+    continue
+  fi
+  echo -e "\e[32mSwitching to directory: $DIR\e[0m"
   cd "$DIR" || exit       # Change to the directory
   echo "Running npm audit fix (breaking changes will need to be addressed manually)"
   npm audit fix --package-lock-only || true
