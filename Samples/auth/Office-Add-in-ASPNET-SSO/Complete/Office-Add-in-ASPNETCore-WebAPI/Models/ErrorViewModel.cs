501f0af2e0d0f07b70c2ce70053a8f820e23fde9
David Chesnut <davech@microsoft.com>
2026-01-26 10:37:39 -0800

    Consolidate to one project

--- Samples/auth/Office-Add-in-ASPNET-SSO/Complete/Office-Add-in-ASPNETCore-WebAPI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Office_Add_in_ASPNETCore_WebAPI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
