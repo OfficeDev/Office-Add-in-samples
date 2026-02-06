07e1d6f2d1b91a7aa09b4a295cd6829a2c50f2be
Elizabeth Samuel <elizs@microsoft.com>
2026-02-06 08:35:10 -0800

    [Excel] (content add-in) Add data visualization sample

---
+++ Samples/excel-content-data-visualization/src/data-binding/data-binding.js
@@ -0,0 +1,63 @@
+﻿(function () {
+    'use strict';
+
+    // The onReady function must be defined for each page in your add-in.
+    Office.onReady(function (info) {
+        shared.initialize();
+
+        document.getElementById("bind-to-existing-data").addEventListener("click", () => bindToExistingData());
+
+        if (dataInsertionSupported()) {
+            document.getElementById("insert-sample-data").addEventListener("click", () => insertSampleData());
+            document.getElementById("insert-data-available").style.display = "block";
+            document.getElementById("insert-data-unavailable").style.display = "none";
+        } else {
+            document.getElementById("insert-data-available").style.display = "none";
+            document.getElementById("insert-data-unavailable").style.display = "block";
+        }
+    });
+
+    // Binds the visualization to existing data.
+    function bindToExistingData() {
+        Office.context.document.bindings.addFromPromptAsync(
+            Office.BindingType.Table,
+            { id: shared.bindingID, sampleData: visualization.generateSampleData() },
+            function (result) {
+                if (result.status === Office.AsyncResultStatus.Succeeded) {
+                    window.location.href = 'home.html';
+                } else {
+                    shared.showNotification(result.error.name, result.error.message);
+                }
+            }
+        );
+    }
+
+    // Checks whether the current application supports setting selected data.
+    function dataInsertionSupported() {
+        return Office.context.document.setSelectedDataAsync &&
+            (Office.context.document.bindings &&
+                Office.context.document.bindings.addFromSelectionAsync);
+    }
+
+    // Inserts sample data into the current selection (if supported).
+    function insertSampleData() {
+        Office.context.document.setSelectedDataAsync(visualization.generateSampleData(),
+            function (result) {
+                if (result.status === Office.AsyncResultStatus.Succeeded) {
+                    Office.context.document.bindings.addFromSelectionAsync(
+                        Office.BindingType.Table, { id: shared.bindingID },
+                        function (result) {
+                            if (result.status === Office.AsyncResultStatus.Succeeded) {
+                                window.location.href = 'home.html';
+                            } else {
+                                shared.showNotification(result.error.name, result.error.message);
+                            }
+                        }
+                    );
+                } else {
+                    shared.showNotification(result.error.name, result.error.message);
+                }
+            }
+        );
+    }
+})();
