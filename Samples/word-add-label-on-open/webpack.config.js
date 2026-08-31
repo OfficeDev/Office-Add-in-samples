05ccb9cbd96c48badbcf73a5f751d1a16b7e04e0
Rick Kirkham <Rick-Kirkham@users.noreply.github.com>
2026-08-31 14:22:36 -0700

    Fix bugs in OnDocumentOpened sample (#1531)
    * Fix bugs in OnDocumentOpened sample
    
    * various typos and format fixes
    
    * image fix
    
    * fix image link
    
    * Apply batched suggestions from code review
    
    Co-authored-by: Alex Jerabek <38896772+AlexJerabek@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Alex Jerabek <38896772+AlexJerabek@users.noreply.github.com>

--- Samples/word-add-label-on-open/webpack.config.js
+++ Samples/word-add-label-on-open/webpack.config.js
@@ -3,6 +3,7 @@
 const devCerts = require("office-addin-dev-certs");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 const urlDev = "https://localhost:3000/";
 const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
@@ -82,6 +83,11 @@ module.exports = async (env, options) => {
       }),
     ],
     devServer: {
+      allowedHosts: "all",
+      static: {
+        directory: path.join(__dirname, "dist"),
+        publicPath: "/public",
+      },
       headers: {
         "Access-Control-Allow-Origin": "*",
       },
