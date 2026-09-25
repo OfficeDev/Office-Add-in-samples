b0cdf0d44cd17342f7dac38459e7e5b6bda855a1
Sam Ramon <15154970+samantharamon@users.noreply.github.com>
2026-09-25 15:26:58 -0700

    Delete outdated samples

--- Samples/Office.TypeScriptAddin/Office.TypeScriptAddinWeb/Scripts/_references.js
+++
@@ -1,8 +0,0 @@
-﻿/// <reference path="jquery-1.9.1.js" />
-/* Required to correctly initalize Office.js for intellisense */
-/// <reference path="_officeintellisense.js" />
-/* Use offline copy of Office.js for intellisense */
-// /// <reference path="office/1/outlook-win32.debug.js" />
-// /// <reference path="office/1/office.js" />
-/* Use online copy of Office.js for intellisense */
-/// <reference path="https://officeapis.public.onecdn.static.microsoft/1/office.js" />
--- Samples/Outlook.MailCRM/DXDemos.Office365/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DXDemos.Office365.MvcApplication" Language="C#" %>
